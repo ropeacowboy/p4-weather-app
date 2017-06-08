e83c04ee06ac10b26b5687787e4676fd088877e6
ropeacowboy <rhatcher1@vcd-ch204-15.eastern.ewu.edu>
2017-06-08 14:06:18 -0700

    second save

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
 //Scripts
+// Get and store Geo Location lat/long coordinates
+navigator.geolocation.getCurrentPosition( 
+  
+  function(position) {
+  
+  // wait a few seconds to receive location
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  
+  console.log( lat, long); 
+
+  
+});
+
+$('button').click();
+
+
+
+// Get and store Geo Location lat/long coordinates
+navigator.geolocation.getCurrentPosition( 
+  
+  function(position) {
+  
+  // wait a few seconds to receive location
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  
+  console.log( lat, long); 
+
+});
